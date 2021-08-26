eafaad895a4d829a2e15ac399bfbccf728a9d9cf
Yoan Thirion <yoan.thirion@gmail.com>
2021-08-26 12:55:54 +0200

    factorize tests

--- scala/src/test/scala/fizzbuzz_should.scala
+++ scala/src/test/scala/fizzbuzz_should.scala
@@ -1,37 +1,32 @@
 import org.scalatest.OptionValues.convertOptionToValuable
 import org.scalatest.flatspec.AnyFlatSpec
 
-import scala.language.postfixOps
-
 class fizzbuzz_should extends AnyFlatSpec {
   it should "return the given number" in {
-    assert(FizzBuzz.convert(1).value == "1")
-    assert(FizzBuzz.convert(67).value == "67")
-    assert(FizzBuzz.convert(82).value == "82")
+    assertFizzBuzzValue(1, 67, 82)(i => i.toString)
   }
 
-  //TODO : parameterize them
   it should "return Fizz for multiples of 3" in {
-    assert(FizzBuzz.convert(3).value == "Fizz")
-    assert(FizzBuzz.convert(66).value == "Fizz")
-    assert(FizzBuzz.convert(99).value == "Fizz")
+    assertFizzBuzzValue(3, 66, 99)(_ => "Fizz")
   }
 
   it should "return Fizz for multiples of 5" in {
-    assert(FizzBuzz.convert(5).value == "Buzz")
-    assert(FizzBuzz.convert(50).value == "Buzz")
-    assert(FizzBuzz.convert(85).value == "Buzz")
+    assertFizzBuzzValue(5, 50, 85)(_ => "Buzz")
   }
 
   it should "return FizzBuzz for multiples of 3 and 5 (multiples of 15)" in {
-    assert(FizzBuzz.convert(15).value == "FizzBuzz")
-    assert(FizzBuzz.convert(30).value == "FizzBuzz")
-    assert(FizzBuzz.convert(45).value == "FizzBuzz")
+    assertFizzBuzzValue(15, 30, 45)(_ => "FizzBuzz")
   }
 
   it should "return None because it is out of range [1, 100]" in {
-    assert(FizzBuzz.convert(0).isEmpty)
-    assert(FizzBuzz.convert(101).isEmpty)
-    assert(FizzBuzz.convert(-1).isEmpty)
+    assertFizzBuzzEmpty(0, 101, -1)
+  }
+
+  private def assertFizzBuzzValue(sample: Int*)(expectedValue: Int => String): Unit = {
+    sample.foreach(i => assert(FizzBuzz.convert(i).value == expectedValue(i)))
+  }
+
+  private def assertFizzBuzzEmpty(sample: Int*): Unit = {
+    sample.foreach(i => assert(FizzBuzz.convert(i).isEmpty))
   }
 }
